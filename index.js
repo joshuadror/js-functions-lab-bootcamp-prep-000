dd733d1ae8d660b8e72d52043add6a272365607e
Joshua Dror <jdror2@gmail.com>
2017-12-13 19:59:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
-/*
+
 function happyHolidaysTo(name) {
   return `Happy holiday, ${name}!`
 }
 function happHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
-
+/*
 function holidayCountdown(){
   return string (${`It's ${days} days until ${holiday}!`})
 }
