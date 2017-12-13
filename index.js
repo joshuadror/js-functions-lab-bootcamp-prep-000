c7578b73c2c1c532a0e7c4b204a648fc105b1d84
Joshua Dror <jdror2@gmail.com>
2017-12-13 20:02:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(name) {
   return `Happy holiday, ${name}!`
 }
 
-function happHolidayTo(holiday, name){
+function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 /*
